29fdbcb88ece7448d7609e32e1ce648038adc672
elwazana <35268928+elwazana@users.noreply.github.com>
2019-11-02 18:22:58 -0400

    Update and rename Kiosk Design MEC 2019 (2).py to Kiosk Design MEC 2019.py

--- Kiosk Design MEC 2019.py
+++ Kiosk Design MEC 2019.py
@@ -1,133 +1,8 @@
+from Restuarants import *
+from Students import *
 
-# coding: utf-8
-
-# In[ ]:
-
-
-class Restuarant:
-    def __init__(self, name):
-        self.name = name
-        self.menu = {}
-        
-    def add_to_menu(self, item, price):
-        self.menu[item] = price
-class Restuarants:
-    def __init__(self, lstRest):
-        self.lst = lstRest
-    def add_to(self, rest):
-        self.lst.append(rest)
-        
-glblRests = Restuarants([])
-rest1 = Restuarant("Creation X")
-rest1.add_to_menu("Chicken Strips",7.29)
-rest1.add_to_menu("Traditional Poutine",6.95)
-rest1.add_to_menu("Butter Chicken Poutine",7.95)
-rest1.add_to_menu("Pulled Pork Poutine",8.75)
-rest1.add_to_menu("Steak N' Eggs",11.75)
-rest1.add_to_menu("Perogies",5.95)
-rest1.add_to_menu("Popcorn Shrimp Dinner",10.95)
-rest1.add_to_menu("Curly Fries",5.25)
-rest1.add_to_menu("Regular Fries",2.99)
-rest1.add_to_menu("Baked Potato",3.40)
-rest1.add_to_menu("Southwest Wrap (chicken/Beef)",7.55)
-rest1.add_to_menu("Falafel & Hummus Wrap",6.99)
-rest1.add_to_menu("Kale Chicken Caesar Wrap",7.50)
-rest1.add_to_menu("Popcorn Shrimp Wrap",8.65)
-rest1.add_to_menu("Buffalo Chicken Wrap",8.25)
-rest1.add_to_menu("Steak Sandwich",8.45)
-rest1.add_to_menu("Texas Grilled Cheese (with bacon, ham, or tomato)",4.25)
-rest1.add_to_menu("Pulled Port Hoagie",8.95)
-rest1.add_to_menu("Tabasco Buffalo Chicken Sanwich",8.25)
-rest1.add_to_menu("Texas B.L.T",4.30)
-
-glblRests.add_to(rest1)
-
-rest2 = Restuarant("Eggcetera")
-rest2.add_to_menu("the hungry marauder",8.99)
-rest2.add_to_menu("the slam dunk",5.95)
-rest2.add_to_menu("build a waffle",6.75)
-rest2.add_to_menu("homestyle panacakes",4.25)
-rest2.add_to_menu("chipotle steak & egss wrap",7.95)
-rest2.add_to_menu("toasted western",4.25)
-rest2.add_to_menu("three egg omelette on a wrap",5.99)
-rest2.add_to_menu("three egg omelette with toast",4.99)
-rest2.add_to_menu("two eggs & toast with protein",4.15)
-rest2.add_to_menu("hashbrown",1.25)
-
-glblRests.add_to(rest2)
-
-rest3 = Restuarant("Global Delights")
-rest3.add_to_menu("Fried Chicken Meal",10.99)
-rest3.add_to_menu("2 Piece Chicken",6.99)
-rest3.add_to_menu("3 Piece Chicken",7.99)
-rest3.add_to_menu("Mac N' Cheese Meal",6.99)
-rest3.add_to_menu("Cajun Pollock",8.99)
-rest3.add_to_menu("Corn Cobettes",1.99)
-rest3.add_to_menu("Malibu Vegtables",2.99)
-rest3.add_to_menu("New Potatoes",1.99)
-rest3.add_to_menu("Smashed Potatoes",2.99)
-rest3.add_to_menu("Mac N' Cheese Side",4.99)
-rest3.add_to_menu("Garlic Stick",1.00)
-rest3.add_to_menu("Jerk Chicken Meal",10.99)
-rest3.add_to_menu("3 Piece Chicken",7.99)
-rest3.add_to_menu("2 Piece Chicken",6.99)
-rest3.add_to_menu("Curried Roti",9.99)
-rest3.add_to_menu("Jamaican Fish Rundown",9.99)
-rest3.add_to_menu("Rice & Beans",2.99)
-rest3.add_to_menu("Teriyaki Chicken",10.99)
-rest3.add_to_menu("Sweet & Sour Chicken",10.99)
-rest3.add_to_menu("Vegetable Fried Rice",9.99)
-rest3.add_to_menu("Stir-Fried Begetables",9.99)
-rest3.add_to_menu("Vegtable Spring Rools",2.25)
-rest3.add_to_menu("Chowmein Noddles",3.99)
-rest3.add_to_menu("Stir-Fried Rice",2.99)
-rest3.add_to_menu("Chicken Quesadilla",10.99)
-rest3.add_to_menu("Veggie & Mushroom Quesadilla",10.99)
-rest3.add_to_menu("Beef Chimichanga Meal",10.99)
-rest3.add_to_menu("Tex-Mex Rice",2.99)
-rest3.add_to_menu("Refired Beans",1.99)
-rest3.add_to_menu("Black Beans",0.99)
-rest3.add_to_menu("Chicken Parmesan",10.99)
-rest3.add_to_menu("Beef Lasagna",10.99)
-rest3.add_to_menu("Vegetable Lasgna",10.99)
-rest3.add_to_menu("Seafood Pasta",8.99)
-rest3.add_to_menu("Cheese Tortellini",8.99)
-
-glblRests.add_to(rest3)
-
-rest4 = Restuarant("HammerTown")
-rest4.add_to_menu("Big Breakfast Crunch",6.45)
-rest4.add_to_menu("Egg Mac",3.15)
-rest4.add_to_menu("Ham N' Egg Croissant",4.99)
-rest4.add_to_menu("Breakfast Bagel",4.99)
-rest4.add_to_menu("The Reuben",9.00)
-rest4.add_to_menu("Roast Beef Sanwich",8.55)
-rest4.add_to_menu("Turkey Bacon Club",7.25)
-rest4.add_to_menu("Grilled Beggie Sanwich",9.00)
-rest4.add_to_menu("Tuna Melt",6.25)
-rest4.add_to_menu("Pho Beef Deep",7.50)
-rest4.add_to_menu("Pesto Chicken",7.50)
-rest4.add_to_menu("Curried Vegetable Roti",6.00)
-rest4.add_to_menu("The Al Capone",8.95)
-rest4.add_to_menu("Ultimate Ham N' Cheese",5.75)
-rest4.add_to_menu("Turkey Havarti",6.00)
-
-glblRests.add_to(rest4)
-
-rest5 = Restuarant("Pizza Pizza")
-rest5.add_to_menu("Cheese Slice",5.49)
-rest5.add_to_menu("Pepporni Slice",6.49)
-rest5.add_to_menu("Veggie Slice",6.49)
-
-glblRests.add_to(rest5)
-
-rest6 = Restuarant("Other")
-rest6.add_to_menu("Bag of Chips",1.99)
-rest6.add_to_menu("Piece of Fruit",2.99)
-rest6.add_to_menu("Pastry",3.99)
-rest6.add_to_menu("Drink",3.99)
-glblRests.add_to(rest6)
-
+# displays menu and selection options for the 
+# restuarant Creation X
 def creationX():
     orderlst = []
     total = 0
@@ -148,7 +23,8 @@ def creationX():
     print("Your Order is: ", orderlst)
     print("Your total is: ", total)
             
-            
+# displays menu and selection options for the 
+# restuarant Eggcetera
 def eggcetra():
     orderlst = []
     total = 0
@@ -169,7 +45,8 @@ def eggcetra():
     print("Your Order is: ", orderlst)
     print("Your total is: ", total)
     
-
+# displays menu and selection options for the 
+# restuarant Global Delights
 def globDel():
     orderlst = []
     total = 0
@@ -190,6 +67,8 @@ def globDel():
     print("Your Order is: ", orderlst)
     print("Your total is: ", total)
 
+# displays menu and selection options for the 
+# restuarant HammerTown
 def hamTown():
     orderlst = []
     total = 0
@@ -209,6 +88,9 @@ def hamTown():
             select = int(input("Would you like to add anything? If yes, press 0, if not press 1"))
     print("Your Order is: ", orderlst)
     print("Your total is: ", total)
+
+# displays menu and selection options for the 
+# restuarant Pizza Pizza   
 def pizzaPizza():
     orderlst = []
     total = 0
@@ -229,7 +111,8 @@ def pizzaPizza():
     print("Your Order is: ", orderlst)
     print("Your total is: ", total)
 
-
+# displays menu and selection options for the 
+# misc items sold at La Piazza
 def otherStuff():
     orderlst = []
     total = 0
@@ -250,7 +133,7 @@ def otherStuff():
     print("Your Order is: ", orderlst)
     print("Your total is: ", total)
     
-
+# displays restuarants avaliable to choose from
 def restMenu():
     restSelection = input("Pick the Restaurant you would like to order from\n 1. CreationX\n 2. Eggcetra\n 3. Global Delights\n 4. HammerTown\n 5. PizzaPizza\n 6. Other\n Press -99 to exit")
     if(restSelection == '-99'):
@@ -271,7 +154,8 @@ def restMenu():
     else:
         print("Invalid Response")
         restMenu()
-        
+ 
+# displays main homescreen of the application
 def main():
     selection = 0
     print("Welcome to La Piazza\n")

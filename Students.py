127b07f3c410ae9412fe5e92b1688fbead34d627
elwazana <35268928+elwazana@users.noreply.github.com>
2019-11-02 17:29:40 -0400

    added file imports

---
+++ Students.py
@@ -0,0 +1,21 @@
+class Student(object): 
+    def __init__(self, name, num):
+        self.name = name
+        self.num = num
+        self.fav = []
+        self.last = []
+    
+    def setFav(self, favs):
+        self.fav = favs
+        
+    def setLast(self, lasTime):
+        self.last = lasTime
+
+class Students:
+
+    def __init__(self, lstStd):
+        self.lst = lstRest
+
+    def add_to(self, std):
+        self.lst.append(rest)
+
